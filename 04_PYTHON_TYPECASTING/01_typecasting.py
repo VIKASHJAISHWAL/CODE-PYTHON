357f04d9d07052fcb31383284187790e1ffa8ea8
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:19:47 +0530

    Add files via upload

---
+++ 04_PYTHON_TYPECASTING/01_typecasting.py
@@ -0,0 +1,21 @@
+a=34
+b="34"
+d=223
+print(a)  
+print(type(a))
+print(b)  
+print(type(b)) 
+# convert string to integer
+c=int(b)  
+print(c) 
+print(type(c))
+e=str(d)  
+print(e) 
+print(type(e))
+# What is Typecasting?
+# Typecasting is the process of converting one data type to another.
+# Python provides built-in functions for typecasting:
+# int(): Converts to integer.
+# float(): Converts to float.
+# str(): Converts to string.
+# bool(): Converts to boolean.
