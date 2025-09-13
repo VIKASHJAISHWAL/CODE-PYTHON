c99e40f9c0c554d8297bd68d3befe236ad7e138f
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:27:38 +0530

    Add files via upload

---
+++ 07_OPERATOR/04_logical_operator.py
@@ -0,0 +1,7 @@
+# 3.Logical Operators:
+
+# and, or, not.
+print(True and False)  # Output: False
+print(True or False)   # Output: True
+print(not True)        # Output: False
+print(not False)       # Output: True
