ddbca810425eadc3d7a73c9c77ec35e745f486d0
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:28:39 +0530

    Add files via upload

---
+++ 07_OPERATOR/06_Identity_operator.py
@@ -0,0 +1,10 @@
+# 6.Identity Operators:
+
+# is, is not.
+x = 10
+y = 10
+print(x is y)  # Output: True
+print(x is not y)  # Output: False
+y=20
+print(x is y)  # Output: False
+print(x is not y) # Output: True
