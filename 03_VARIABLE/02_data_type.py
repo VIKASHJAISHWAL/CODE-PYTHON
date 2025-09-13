45bdaa6258f9c10dbebde9561953d5c1ead1893b
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:18:01 +0530

    Add files via upload

---
+++ 03_VARIABLE/02_data_type.py
@@ -0,0 +1,25 @@
+# Data Types in Python?
+# Python supports several built-in data types:
+
+# Integers (int): Whole numbers (e.g., 10, -5).
+# Floats (float): Decimal numbers (e.g., 3.14, -0.001).
+# Strings (str): Text data enclosed in quotes (e.g., "Hello", 'Python').
+# Booleans (bool): Represents True or False.
+# Lists: Ordered, mutable collections (e.g., [1, 2, 3]).
+# Tuples: Ordered, immutable collections (e.g., (1, 2, 3)).
+# Sets: Unordered collections of unique elements (e.g., {1, 2, 3}).
+# Dictionaries: Key-value pairs (e.g., {"name": "Alice", "age": 25}).
+print(type(10))       # Output: <class 'int'>
+print(type("Hello"))  # Output: <class 'str'>
+age=8
+print(age)
+print(type(age))
+cgpa=8.5
+print(cgpa)
+print(type(cgpa))
+is_student=True#can also be False
+print(is_student)
+print(type(is_student))
+name="vikash"
+print(name)
+print(type(name))
