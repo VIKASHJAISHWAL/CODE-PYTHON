20bd865fb59cfcf65d7905d6868fb2b985d1b6be
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:30:44 +0530

    Add files via upload

---
+++ PRECTICE_SET_01/Question_2.py
@@ -0,0 +1,5 @@
+# Q2: Print a Poem
+# Write a program that prints the following poem using a single print() statement:
+
+# (Hint: Use \n for a new line.)
+print("Twinkle, twinkel little star,\nHow I wonder what you are!\n")
