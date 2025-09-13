39207c3eaf684ea4bd1713ca9b7c3aa09956bcae
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:22:31 +0530

    Add files via upload

---
+++ 05_USER_INPUT/01_input.py
@@ -0,0 +1,31 @@
+# a=input("Enter a number:")
+# print(a) 
+# b=input("Enter any name:")
+# print(b)
+
+
+# a=input("Enter first number:")
+# b=int(a)  
+# print(b+3)  
+
+x=input("Enter firdt number:")
+y=input("Enter second number:")
+print(x+y)
+print("-----------------------------")
+# x=input("Enter first number:")
+w=int(x)
+# y=input("Enter second number:")
+v=int(y) 
+print(w+v)
+print("-----------------------------") 
+x=int(input("Enter first number:"))
+y=int(input("Enter second number:"))
+print(x+y)
+
+
+# Using the input() Function
+# The input() function allows you to take user input from the keyboard.
+# By default, input() returns a string. You can convert it to other data types as needed.
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hello {name}, you are {age} years old.")
