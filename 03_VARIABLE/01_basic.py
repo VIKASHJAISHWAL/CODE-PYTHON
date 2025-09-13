e2b38a3da88334595151a9cdcb7a24cab75dbfce
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:17:44 +0530

    Add files via upload

---
+++ 03_VARIABLE/01_basic.py
@@ -0,0 +1,22 @@
+# What are Variables?
+# Variables are used to store data that can be used and manipulated in a program.
+# A variable is created when you assign a value to it using the = operator.
+# Example:
+name = "Alice" # string
+age = 25 # integer
+height = 5.6 # float
+
+# Variable Naming Rules:
+# Variable names can contain letters, numbers, and underscores.
+# Variable names must start with a letter or underscore.
+# Variable names are case-sensitive.
+# Avoid using Python keywords as variable names (e.g., print, if, else).
+
+# Best Practices:
+# Use descriptive names that reflect the purpose of the variable.
+# Use lowercase letters for variable names.
+# Separate words using underscores for readability (e.g., first_name, total_amount).
+# 34age=4 #Invalid variable name (start with a number)
+age=32 #valid variable name because it start with a letter
+_age=40#valid variable name because it start with a underscore
+a_b_=50#valid variable name because it contain letters and underscores
