38d3c8b3d17a5211a8908937ea3acf1a5311910f
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:34:00 +0530

    Add files via upload

---
+++ PRECTICE_SET_01/Question_8.py
@@ -0,0 +1,11 @@
+# Q8: Operator Challenge
+# Write a program that:
+
+# Takes an integer as input from the user.
+# Prints the square and cube of that number.
+num=int(input("Enter a number:\n"))
+print("Square of", num, "is:", num**2)
+print("Cube of", num, "is:", num**3)
+a=int(input("Enter a number:\n"))
+print("Squre is:", a**2)
+print("Cube is:", a**3)
