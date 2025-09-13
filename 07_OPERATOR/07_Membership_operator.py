93eb61a3e20038ef812ac23643cde53f31fccde4
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:28:59 +0530

    Add files via upload

---
+++ 07_OPERATOR/07_Membership_operator.py
@@ -0,0 +1,9 @@
+# 5.Membership Operators:
+
+# in, not in.
+fruits = ["apple", "banana", "cherry"]
+print("banana" in fruits)  # Output: True
+print("orange" not in fruits)  # Output: True
+print("apple" in fruits)       # Output: True
+print("grape" not in fruits)   # Output: True
+print("cherry" in fruits)      # Output: True
