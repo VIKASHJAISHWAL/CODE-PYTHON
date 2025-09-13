d9a315994f968899bd0b28f1a2f8f13a558ca577
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:31:05 +0530

    Add files via upload

---
+++ PRECTICE_SET_01/Question_3.py
@@ -0,0 +1,15 @@
+# Q3: Variables & Data Types
+
+# Create variables to store:
+
+# Your name (string)
+# Your age (integer)
+# Your height in meters (float)
+# A boolean value representing whether you are a student
+# Print all of them in one line.
+name="monika"
+age=20
+height=5.7
+is_student=True
+print(name,age,height,is_student)
+print("The name of student is " + name + ". She is " + str(age) + " years old." + " She is " + str(height) + " cms tall." + " Student is " + str(is_student))
