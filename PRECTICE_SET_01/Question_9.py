069b7436b1d2d00f71354499e9835cee70d672a9
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:34:26 +0530

    Add files via upload

---
+++ PRECTICE_SET_01/Question_9.py
@@ -0,0 +1,12 @@
+# Q9: Quick Quiz (True/False)
+# Mark each as True or False:
+
+# Python code must always end with a semicolon ;
+# The # symbol is used for comments in Python
+# "123" and 123 are the same in Python
+# The * operator is used for multiplication
+# \n creates a new line
+# Variables in Python can start with numbers
+# int("10") + 5 gives 15
+print("False")
+print("True")
