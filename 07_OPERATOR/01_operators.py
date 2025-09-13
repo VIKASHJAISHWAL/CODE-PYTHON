2c9e1b3f4d27fa6b1e6b9a44404e8f90f648cd31
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:26:34 +0530

    Add files via upload

---
+++ 07_OPERATOR/01_operators.py
@@ -0,0 +1,62 @@
+a=34 
+
+
+b=2  
+
+print("a+b = ",a+b)  
+print("a-b = ",a-b)  
+print("a*b = ",a*b)  
+print("a/b = ",a/b)  
+print("a%b = ",a%b)  
+print("a//b = ",a//b)  
+
+# Types of Operators:
+
+# 1.Arithmetic Operators:
+
+# + (Addition), - (Subtraction), * (Multiplication), / (Division), % (Modulus), ** (Exponentiation), // (Floor Division).
+print(10 + 5)   # Output: 15
+print(10 ** 2)  # Output: 100
+
+# 2.Comparison Operators:
+
+# == (Equal), != (Not Equal), > (Greater Than), < (Less Than), >= (Greater Than or Equal), <= (Less Than or Equal).
+print(10 > 5)   # Output: True
+print(10 == 5)  # Output: False
+
+# 3.Logical Operators:
+
+# and, or, not.
+print(True and False)  # Output: False
+print(True or False)   # Output: True
+print(not True)        # Output: False
+
+# 4.Assignment Operators:
+
+# =, +=, -=, *=, /=, %=, **=, //=.
+x = 10
+x += 5  # Equivalent to x = x + 5
+print(x)  # Output: 15
+
+# 5.Membership Operators:
+
+# in, not in.
+fruits = ["apple", "banana", "cherry"]
+print("banana" in fruits)  # Output: True
+
+
+# 6.Identity Operators:
+
+# is, is not.
+x = 10
+y = 10
+print(x is y)  # Output: True
+print(x is not y)  # Output: False
+
+# Summary:
+
+# Variables store data, and Python supports multiple data types.
+# Typecasting allows you to convert between data types.
+# Use input() to take user input and print() to display output.
+# Comments and escape sequences help make your code more readable.
+# Python provides a variety of operators for performing operations on data.
