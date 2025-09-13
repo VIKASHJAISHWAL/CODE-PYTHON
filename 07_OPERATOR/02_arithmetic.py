ebc195b9a6a74e3c8e453b7f7e6b547642b82450
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:26:56 +0530

    Add files via upload

---
+++ 07_OPERATOR/02_arithmetic.py
@@ -0,0 +1,11 @@
+
+# 1.Arithmetic Operators:
+
+# + (Addition), - (Subtraction), * (Multiplication), / (Division), % (Modulus), ** (Exponentiation), // (Floor Division).
+print(10 + 5)   # Output: 15
+print(10 ** 2)  # Output: 100
+print(10 - 5)   # Output: 5
+print(10 * 5)   # Output: 50
+print(10 / 5)   # Output: 2.0
+print(10 % 2)   # Output: 0
+print(10 // 3)  # Output: 3
