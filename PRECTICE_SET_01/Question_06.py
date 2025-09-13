52e5a824e3f78ca418abd926fa620da47f9813a2
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:32:10 +0530

    Add files via upload

---
+++ PRECTICE_SET_01/Question_06.py
@@ -0,0 +1,18 @@
+# Q6: Simple Calculator
+# Write a program that:
+
+# Takes two numbers as input from the user.
+
+# Prints their:
+
+# Sum
+# Difference
+# Product
+# Quotient
+
+n1=input("Enter first number:\n")
+n2=input("Enter second number:\n")
+print("Sum of two number is:", int(n1)+int(n2))
+print("Difference of two number is:", int(n1)-int(n2))
+print("Product of two number is:", int(n1)*int(n2))
+print("Quotient of two number is:", int(n1)/int(n2))
