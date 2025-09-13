88729b7d3d01b27fa311b9ea567dd9c65aa8c3c7
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:31:51 +0530

    Add files via upload

---
+++ PRECTICE_SET_01/Question_5.py
@@ -0,0 +1,8 @@
+# Q5: Taking User Input
+# Write a program that:
+
+# Asks the user for their favorite food.
+# Prints:
+
+food=input("What is your favorite food?\n")
+print("WOW! i also like " + food)
