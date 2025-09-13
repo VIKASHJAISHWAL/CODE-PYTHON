248e1d722427ec203fd747b3ed0c0c14c313c790
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-13 13:27:16 +0530

    Add files via upload

---
+++ 07_OPERATOR/03_conditional_operator.py
@@ -0,0 +1,8 @@
+#  2.Comparison Operators:
+
+# == (Equal), != (Not Equal), > (Greater Than), < (Less Than), >= (Greater Than or Equal), <= (Less Than or Equal).
+print(10 > 5)   # Output: True
+print(10 == 5)  # Output: False
+print(10 < 5)   # Output: False
+print(10 >= 5)  # Output: True
+print(10 <= 5)  # Output: False
